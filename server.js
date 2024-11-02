d38b5c36380f6611441b494d796a9efa1439babc
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-02 00:43:41 -0400

    removed bodyParser-deprecated. removed options object (useNewUrlParser, useUnifiedTopology), updated mongodb query  functions

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-var db, collection;
+let db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url =
+  'mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true';
+const dbName = 'demo';
 
 app.listen(3000, () => {
-    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    MongoClient.connect(url, (error, client) => {
         if(error) {
             throw error;
         }
@@ -18,44 +18,58 @@ app.listen(3000, () => {
     });
 });
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
-  })
-})
+  db.collection('messages')
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      res.render('index.ejs', { messages: result });
+    });
+});
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+  const { name, msg } = req.body;
+  db.collection('messages').insertOne(
+    { name, msg, thumbUp: 0, thumbDown: 0 },
+    (err, result) => {
+      if (err) return console.log(err);
+      console.log('saved to database');
+      res.redirect('/');  
+    }
+  );
+});
 
 app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
+  const { name, msg, thumbUp } = req.body;
+  db.collection('messages').updateOne(
+    { name, msg },
+    {
+      $set: {
+        thumbUp: thumbUp + 1,
+      },
+    },
+    {
+      sort: { _id: -1 },
+      upsert: true,
+    },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
     }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+  );
+});
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('Message deleted!')
-  })
-})
+  db.collection('messages').deleteOne(
+    { name: req.body.name, msg: req.body.msg },
+    (err, result) => {
+      if (err) return res.send(500, err);
+      res.send('Message deleted!');
+    }
+  );
+});
