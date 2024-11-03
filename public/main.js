d2212fe98083a46800eedf466bb41602ce1ccb03
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-03 14:08:21 -0500

    Added thumbs down functionality.

--- public/main.js
+++ public/main.js
@@ -1,8 +1,13 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+const thumbUp = document.getElementsByClassName("fa-thumbs-up");
+const trash = document.getElementsByClassName("fa-trash");
+const thumbDown = document.querySelectorAll('.fa-thumbs-down'); 
+
+const thumbDownArray = Array.from(thumbDown); 
+
 
 Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
+  element.addEventListener('click', function () {
+    console.log('thumb up clicked');
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
@@ -12,7 +17,7 @@ Array.from(thumbUp).forEach(function(element) {
           body: JSON.stringify({
             'name': name,
             'msg': msg,
-            'thumbUp':thumbUp
+            'thumbUp': thumbUp, 
           })
         })
         .then(response => {
@@ -26,7 +31,7 @@ Array.from(thumbUp).forEach(function(element) {
 });
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
+  element.addEventListener('click', function () {
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         fetch('messages', {
@@ -43,3 +48,42 @@ Array.from(trash).forEach(function(element) {
         })
       });
 });
+
+// structure: 
+//  <li class="message">
+//       <span><%= messages[i].name %></span>
+//       <span><%= messages[i].msg %></span>
+//       <span><%= messages[i].thumbUp %></span>
+//       <span><i class="fa fa-thumbs-up" aria-hidden="true"></i></span>
+//       <span><i class="fa fa-thumbs-down" aria-hidden="true"></i></span>
+//       <span><i class="fa fa-trash" aria-hidden="true"></i></span>
+//     </li>
+
+thumbDownArray.forEach(el => {
+  el.addEventListener('click', () => {
+    console.log('click down');
+    const name = el.parentNode.parentNode.childNodes[1].textContent; 
+    const msg = el.parentNode.parentNode.childNodes[3].innerText;
+    const thumbDown = parseFloat(el.parentNode.parentNode.childNodes[9].innerText); 
+    console.log({thumbDown})
+    console.log(name, msg); 
+    fetch('messages', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        msg,
+        thumbDown,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+      })
+      .then((data) => {
+        console.log(data); //we never see this cause the window reloads quickly
+        window.location.reload(true);
+      });
+  
+
+  })
+})
